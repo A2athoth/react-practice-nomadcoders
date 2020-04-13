a5763ddd2243b8516740f099d9ab62b92b9e1742
j9uery <j9uery@gmail.com>
2020-04-14 02:35:11 +0900

    data fetch 배우기 전 사전준비

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        movie: []
+    };
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false})
+        }, 6000);
+    };
+
     render() {
+        const { isLoading } = this.state;       // es6의 마법
         return (
             <div>
-                
+                {isLoading ? "Loading" : "We are ready"}
             </div>
         );
     }
