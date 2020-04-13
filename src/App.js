a2738c1b1c7337a8a6f1788b99a0d0d03ece13eb
j9uery <j9uery@gmail.com>
2020-04-14 01:17:39 +0900

    prop체크용 propTypes 설치 및 활용

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ name, desc }) {
+function Bucket({ name, desc, score }) {
     return <div>
         <h1>I like { name }</h1>
         <div>I'm the { desc }</div>
+        <h3>rating : { score }</h3>
     </div>
+}
+
+Bucket.propTypes = {    // 이건 꼭 !!!! propTypes로 해야 react가 체크함
+    name: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired
+    // 앞의 키값을 바꾸거나, 뒤쪽 값 타입을 바꾸면 오류가 난다.
+    // isRequire로 필수값 여부도 체크
+    // 이외에도 옵션은 많음
 }
 
 const testDatas = [
         {
             id: 0,
             title: "A",
-            type: "Arch"
+            type: "Arch",
+            rating: 5
         },
         {
             id: 1,
             title: "B",
-            type: "Bear"
+            type: "Bear",
+            rating: 3
         },
         {
             id: 2,
             title: "C",
-            type: "Cute"
+            type: "Cute",
+            rating: 5
         }
     ];
 
@@ -30,7 +44,7 @@ function App() {
       <div>
           <h1>Hello</h1>
           {testDatas.map(item => (
-              <Food key={item.id} name={item.title} desc={item.type} />
+              <Bucket key={item.id} name={item.title} desc={item.type} score={item.rating} />
           ))}
       </div>
   );
