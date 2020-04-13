13f27ab967672c5d32a0f157186b0e8b0e0620db
j9uery <j9uery@gmail.com>
2020-04-14 01:25:42 +0900

    state 포함 기본형

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 
 // 클래스 컴포넌트. function이 아니기에 return이 없고, import한 React에서 받아온 render method가 있음
 class App extends React.Component{
+    state = {
+
+    }
     render(){
         return <h1>I'm a class component.</h1>
     }
