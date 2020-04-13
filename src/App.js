2a03270a47346fcf9a53e33699dc8ab531248f53
j9uery <j9uery@gmail.com>
2020-04-14 02:27:34 +0900

    lifeCycle method 일부(componentDidMount, componentDidUpdate) 예제

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 // 클래스 컴포넌트. function이 아니기에 return이 없고, import한 React에서 받아온 render method가 있음
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        console.log("hello");
+    }
     state = {
         count: 0
     }
 
     add = () => {
-        this.setState(current => ({count: current.count+1}));
+        this.setState(current => ({count: current.count + 1 }));
     };
     minus = () => {
-        this.setState(current => ({count: current.count-1}));
+        this.setState(current => ({count: current.count - 1 }));
     };
 
-    render(){
+    componentDidMount() {
+        console.log("component rendered");
+    }
+
+    componentDidUpdate() {
+        console.log("component updated");
+    }
+
+    render() {
+        console.log("im rendering");
         return (
             <div>
                 <h1>The number is : {this.state.count}</h1>
