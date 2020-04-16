0333846e738dfc815ac7ad838c1594f3646834cd
j9uery <j9uery@gmail.com>
2020-04-17 02:43:46 +0900

    id는 빼도 된다.

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // 컴포넌트가 state가 필요없으면 굳이 클래스 컴포넌트여야 할 이유가 없다.
-function Movie({id, year, title, summary, poster}) {
+function Movie({year, title, summary, poster}) {
     return <div className="movie">
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
