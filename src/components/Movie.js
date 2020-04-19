1a2f3664c915be080d9d5b93f14eef046b7e48c9
a2athoth <blockchain.azathoth@gmail.com>
2020-04-19 21:28:38 +0900

    movie-detail 대신 movie/:id 사용

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
-import "./Movie.css"
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-// 컴포넌트가 state가 필요없으면 굳이 클래스 컴포넌트여야 할 이유가 없다.
-function Movie({year, title, summary, poster, genres}) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return (
-        <Link to={{
-            pathname: "/movie-detail",
-            state: {
-                year,
-                title,
-                summary,
-                poster,
-                genres
-            }
-        }}>
-            <div className="movie">
-                <img src={poster} alt={title} title={title}/>
+        <div className="movie">
+            <Link
+                to={{
+                    pathname: `/movie/${id}`,
+                    state: {
+                        year,
+                        title,
+                        summary,
+                        poster,
+                        genres
+                    }
+                }}
+            >
+                <img src={poster} alt={title} title={title} />
                 <div className="movie__data">
                     <h3 className="movie__title">{title}</h3>
                     <h5 className="movie__year">{year}</h5>
@@ -30,8 +31,8 @@ function Movie({year, title, summary, poster, genres}) {
                     </ul>
                     <p className="movie__summary">{summary.slice(0, 180)}...</p>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
 
@@ -42,6 +43,6 @@ Movie.propTypes = {
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired
-}
+};
 
 export default Movie;
