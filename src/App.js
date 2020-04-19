7d77714a1727140f0fb47c21a063b657dd37670c
a2athoth <blockchain.azathoth@gmail.com>
2020-04-19 18:45:38 +0900

    router 작동방식. 이러면 /home/introduction 했을때 home이 두번 렌더링 된다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home"
 import About from "./routes/About"
 
 function App() {
     return <HashRouter>
-        <Route path="/about" component={About} />
+        <Route path="/home">
+            <h1>Home</h1>
+        </Route>
+        <Route path="/home/introduction">
+            <h1>Home</h1>
+        </Route>
+        <Route path="/about">
+            <h1>About</h1>
+        </Route>
     </HashRouter>
 }
 
